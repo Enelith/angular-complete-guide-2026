682efcc0daf449c5348a671e36c9ceca461d4ac9
VIVERIS\j.vinh <jonathan.vinh@viveris.fr>
2026-03-02 14:23:39 +0100

    02-17 Styling the Header Component & Adding an Image

--- 02 - Angular Essentials, Components, Templates, Services & More/02_11_new-starting-project/src/app/header.ts
+++ 02 - Angular Essentials, Components, Templates, Services & More/02_11_new-starting-project/src/app/header.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   standalone: true, // With Angular 20+, this property is set to TRUE by default (so in our case, it's useless)
+  styleUrl: 'header.scss',
   templateUrl: './header.html'
 })
 export class HeaderComponent {
