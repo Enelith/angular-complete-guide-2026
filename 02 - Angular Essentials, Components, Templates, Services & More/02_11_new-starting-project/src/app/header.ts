95b59ed118d10b23902ffac2f9280f0f4352246e
VIVERIS\j.vinh <jonathan.vinh@viveris.fr>
2026-03-02 13:42:54 +0100

    02-15 Configuring the Custom Component

--- 02 - Angular Essentials, Components, Templates, Services & More/02_11_new-starting-project/src/app/header.ts
+++ 02 - Angular Essentials, Components, Templates, Services & More/02_11_new-starting-project/src/app/header.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
 
-@Component()
+@Component({
+  selector: 'app-header',
+  standalone: true, // With Angular 20+, this property is set to TRUE by default (so in our case, it's useless)
+  templateUrl: './header.html'
+})
 export class HeaderComponent {
+  // TODO: How do we use it now?
 
 }
