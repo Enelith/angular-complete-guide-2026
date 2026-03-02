2a8da8a4354d65634e4bd827f5a5aba2a0756944
VIVERIS\j.vinh <jonathan.vinh@viveris.fr>
2026-03-02 14:52:55 +0100

    02-21 Storing Data in a Component Class

--- 02 - Angular Essentials, Components, Templates, Services & More/02_11_new-starting-project/src/app/user/user.ts
+++ 02 - Angular Essentials, Components, Templates, Services & More/02_11_new-starting-project/src/app/user/user.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
 
+// Importing dummy users
+import { DUMMY_USERS } from '../dummy-users';
+
+// Having a random number generated, based on the Dummy Users collection
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
+
 @Component({
   selector: 'app-user',
   imports: [],
   templateUrl: './user.html',
   styleUrl: './user.scss',
 })
-export class UserComponent {}
+export class UserComponent {
+  // Will display the random user, but how to use it in the template now?
+  selectedUser = DUMMY_USERS[randomIndex]
+}
