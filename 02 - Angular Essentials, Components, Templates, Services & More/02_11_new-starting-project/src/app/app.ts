3b79feec195819eacbcf36b69187ab4ee5a398e8
VIVERIS\j.vinh <jonathan.vinh@viveris.fr>
2026-03-02 14:38:41 +0100

    02-18 Managing & Creating Components with the Angular CLI

--- 02 - Angular Essentials, Components, Templates, Services & More/02_11_new-starting-project/src/app/app.ts
+++ 02 - Angular Essentials, Components, Templates, Services & More/02_11_new-starting-project/src/app/app.ts
@@ -1,14 +1,16 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
 
-import { HeaderComponent } from './header';
+import { HeaderComponent } from './header/header';
+import { UserComponent } from './user/user';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [
+    HeaderComponent,
+    UserComponent
+  ],
   templateUrl: './app.html',
-  styleUrl: './app.sass',
+  styleUrl: './app.scss',
 })
 export class App {
-  protected readonly title = signal('02_11_new-starting-project');
 }
