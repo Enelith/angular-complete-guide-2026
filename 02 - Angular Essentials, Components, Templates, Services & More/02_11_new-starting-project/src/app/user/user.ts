efce8af08f259b114b0ef2967d1677329a8b4e5c
VIVERIS\j.vinh <jonathan.vinh@viveris.fr>
2026-03-02 16:51:02 +0100

    02-32 Required & Optional Inputs

--- 02 - Angular Essentials, Components, Templates, Services & More/02_11_new-starting-project/src/app/user/user.ts
+++ 02 - Angular Essentials, Components, Templates, Services & More/02_11_new-starting-project/src/app/user/user.ts
@@ -7,12 +7,17 @@ import { Component, Input } from '@angular/core';
   styleUrl: './user.scss',
 })
 export class UserComponent {
-  // @Input() marks this property as settable from outside this component.
-  // Adding "!" after the `avatar` variable tells TypeScript that we know that this variable will be set to some value,
-  // even though TypeScript can't see it in this code
-  @Input() avatar!: string;
+  /**
+   * 1) @Input() marks this property as settable from outside this component.
+   *
+   * 2) Adding "!" after the `avatar` variable tells TypeScript that we know that this variable will be set to some value,
+   * even though TypeScript can't see it in this code.
+   *
+   * 3) Adding a "required: true" configuration to @Input will throw an error if the component is called without all required fields declared.
+   */
+  @Input({ required: true }) avatar!: string;
 
-  @Input() name!: string;
+  @Input({ required: true }) name!: string;
 
   get imagePath() {
     return 'assets/users/' + this.avatar;
