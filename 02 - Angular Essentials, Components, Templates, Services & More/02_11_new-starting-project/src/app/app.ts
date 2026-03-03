91bc608102fcdcc70a52e06693be4894286e955d
VIVERIS\j.vinh <jonathan.vinh@viveris.fr>
2026-03-03 14:04:35 +0100

    02.38 Exercice: Create a Configurable Component

--- 02 - Angular Essentials, Components, Templates, Services & More/02_11_new-starting-project/src/app/app.ts
+++ 02 - Angular Essentials, Components, Templates, Services & More/02_11_new-starting-project/src/app/app.ts
@@ -1,19 +1,26 @@
 import { Component } from '@angular/core';
 
 import { HeaderComponent } from './header/header';
+import { TasksComponent } from './tasks/tasks';
 import { UserComponent } from './user/user';
 import { DUMMY_USERS } from './dummy-users';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, TasksComponent, UserComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
 export class App {
   users = DUMMY_USERS;
 
+  targetUserId: string = 'u1';
+
+  get targetedUser() {
+    return this.users.find((user) => (user.id === this.targetUserId))!;
+  }
+
   onAppHandleSelectedUser(id: string) {
-    console.log(id);
+    this.targetUserId = id;
   }
 }
