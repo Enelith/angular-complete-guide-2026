0022ba885ef958057b74d5fdccf233bccd28b45a
VIVERIS\j.vinh <jonathan.vinh@viveris.fr>
2026-03-04 11:54:06 +0100

    02.54 Signals & Two-Way-Binding

--- 02 - Angular Essentials, Components, Templates, Services & More/02_40_same-project-cleaner/src/app/tasks/new-task/new-task.ts
+++ 02 - Angular Essentials, Components, Templates, Services & More/02_40_same-project-cleaner/src/app/tasks/new-task/new-task.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 /**
@@ -14,9 +14,20 @@ export class NewTaskComponent {
   // "void" here means there will be no data emitted
   @Output() cancelling = new EventEmitter<void>();
 
+  /**
+   * "Normal" version, using Two-Way-Binding:
+   * [(ngModel)] = "enteredTitle_signals"
+   */
   enteredTitle = '';
   enteredSummary = '';
   enteredDate = '';
+
+  /**
+   * Signals version, the HTML stays the same, [(ngModel)] will still be functioning as normal,
+   * Difference being, enteredTitle will be registered in HTML as a WritableSignal<string> instead of a string:
+   * [(ngModel)] = "enteredTitle_signals"
+   */
+  enteredTitle_signals = signal('');
 
   onCancel() {
     this.cancelling.emit();
