a8fd10842ae7efbe1b2c5ec088a5b81cfed120f7
VIVERIS\j.vinh <jonathan.vinh@viveris.fr>
2026-03-04 16:43:52 +0100

    02.63 Using localStorage for Data Storage

--- 02 - Angular Essentials, Components, Templates, Services & More/02_40_same-project-cleaner/src/app/tasks/tasks.service.ts
+++ 02 - Angular Essentials, Components, Templates, Services & More/02_40_same-project-cleaner/src/app/tasks/tasks.service.ts
@@ -38,6 +38,15 @@ export class TasksService {
     },
   ];
 
+  constructor() {
+    // Note that localStorage always stores its items as string, so it needs to be converted as JSON first in order to assign it to 'this.tasks'
+    const tasks = localStorage.getItem('tasks');
+
+    if (tasks) {
+      this.tasks = JSON.parse(tasks);
+    }
+  }
+
   getUserTasks(userId: string) {
     return this.tasks.filter((task) => task.userId === userId);
   }
@@ -51,9 +60,15 @@ export class TasksService {
       summary: taskData.summary,
       dueDate: taskData.dueDate,
     });
+    this.saveTasks();
   }
 
   removeTask(taskId: string) {
     this.tasks = this.tasks.filter((task) => task.id !== taskId);
+    this.saveTasks();
+  }
+
+  private saveTasks() {
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
   }
 }
