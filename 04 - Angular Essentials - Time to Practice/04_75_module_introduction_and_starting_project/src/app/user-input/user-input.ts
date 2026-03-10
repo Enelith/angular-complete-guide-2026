832ff23c6a332bed22c5c82894f7c952af9834aa
VIVERIS\j.vinh <jonathan.vinh@viveris.fr>
2026-03-10 11:44:46 +0100

    04.79 Handling Form Submission

--- 04 - Angular Essentials - Time to Practice/04_75_module_introduction_and_starting_project/src/app/user-input/user-input.ts
+++ 04 - Angular Essentials - Time to Practice/04_75_module_introduction_and_starting_project/src/app/user-input/user-input.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user-input',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './user-input.html',
   styleUrl: './user-input.scss',
 })
-export class UserInput {}
+export class UserInput {
+  protected onSubmit() {
+    console.log('submit');
+  }
+}
