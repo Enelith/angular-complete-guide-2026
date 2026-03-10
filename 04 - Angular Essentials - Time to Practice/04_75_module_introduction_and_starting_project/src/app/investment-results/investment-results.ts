284b63e4a6532a6ad73c946b814a0295064c580a
VIVERIS\j.vinh <jonathan.vinh@viveris.fr>
2026-03-10 15:22:08 +0100

    04.86 Formatting Output with a Pipe

--- 04 - Angular Essentials - Time to Practice/04_75_module_introduction_and_starting_project/src/app/investment-results/investment-results.ts
+++ 04 - Angular Essentials - Time to Practice/04_75_module_introduction_and_starting_project/src/app/investment-results/investment-results.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
+import { CurrencyPipe } from '@angular/common';
 
 import { InvestmentResults } from '../investment-results.model';
 
 @Component({
   selector: 'app-investment-results',
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './investment-results.html',
   styleUrl: './investment-results.scss',
 })
