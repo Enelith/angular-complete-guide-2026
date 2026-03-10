11bdb25b85817381d36c8e5cf4bf784aaacd1e9d
VIVERIS\j.vinh <jonathan.vinh@viveris.fr>
2026-03-10 11:41:17 +0100

    04.78 Adding a User Input Component

--- 04 - Angular Essentials - Time to Practice/04_75_module_introduction_and_starting_project/src/app/app.component.ts
+++ 04 - Angular Essentials - Time to Practice/04_75_module_introduction_and_starting_project/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+
 import { Header } from './header/header';
+import { UserInput } from './user-input/user-input';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [Header],
+  imports: [Header, UserInput],
 })
 export class AppComponent {}
