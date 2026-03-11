b271e567646af1272c880fe21301437809116cec
VIVERIS\j.vinh <jonathan.vinh@viveris.fr>
2026-03-11 14:10:35 +0100

    06.101 Component Inputs: Repetition

--- 06 - Components & Templates - Deep Dive/06_97_starting_project_and_an_opportunity_for_smaller_components/src/app/dashboard/dashboard-item/dashboard-item.ts
+++ 06 - Components & Templates - Deep Dive/06_97_starting_project_and_an_opportunity_for_smaller_components/src/app/dashboard/dashboard-item/dashboard-item.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard-item',
@@ -6,4 +6,14 @@ import { Component } from '@angular/core';
   templateUrl: './dashboard-item.html',
   styleUrl: './dashboard-item.scss',
 })
-export class DashboardItem {}
+export class DashboardItem {
+  // DECORATOR VERSION
+  /*
+  @Input({ required: true }) image!: { src: string; alt: string };
+  @Input({ required: true }) title!: string;
+  */
+
+  // USING 'INPUT' FUNCTION (only available with Angular@17.1+)
+  image = input.required<{ src: string; alt: string }>(); // In this case, '{ src: string; alt: string}' is a GENERIC TYPE
+  title = input.required<string>();
+}
